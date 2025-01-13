6c404491ba1dcae18ae17b5c7f7223d3f3908e12
Luca Malandrino <malandrinoluca3@gmail.com>
2025-01-13 19:00:12 +0100

    test

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ taskContainer.innerHTML += tasks;
 
 
 function openPopup() {
-    popup.classList.replace("closed", "open");
+    popup.classList.replace("closed", "open");pppp
 }
 
 function closePopup() {
